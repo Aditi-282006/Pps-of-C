c5dd6a71f9e706adc418679e8fcbca9528c7c916
Aditi satve <satveaditi429@gmail.com>
2025-05-02 10:31:34 +0530

    Update and rename Addition.c to Positive or negative.c

---
+++ Positive or negative.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+  int num;
+
+  printf("Enter The Number\t");
+  scanf("%d",&num);
+
+  if(num>0)
+  {
+   printf("The Number %d Is Positive",num);
+  }
+
+  else
+  {
+   printf("The Number %d Is Negative",num);
+  }
+
+    return 0;
+}
+
+  
