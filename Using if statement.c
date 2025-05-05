076bd2c3fd711d2e0be9087654e46ea0c04b1b9e
Aditi satve <satveaditi429@gmail.com>
2025-05-05 16:35:53 +0530

    Create Using if statement.c

---
+++ Using if statement.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    
+    printf("Enter the number\t");
+    scanf("%d",&num);
+    
+    if(num>3)
+    {
+        printf("\nyes\t");
+    }
+    
+    printf("\nEnd of program\t");
+    
+    return 0;
